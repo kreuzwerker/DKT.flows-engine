ee46a5721882a9f63f5431defd685e6f5698ecee
johdirr <johannesdirr@me.com>
2016-12-20 16:05:47 +0100

    more workflow tests

--- src/workflows/test.js
+++ src/workflows/test.js
@@ -1,6 +1,5 @@
 const Fs = require('fs')
 const Path = require('path')
-const settings = require('../../settings.js')
 
 
 describe('Workflows', function () {
@@ -8,33 +7,46 @@ describe('Workflows', function () {
 
   workflows.forEach((flow) => {
     const name = flow.split('.')[0]
-    let json
-
-    before(function () {
-      json = Fs.readFileSync(Path.join(__dirname, flow)).toString()
-    })
-
+    const json = Fs.readFileSync(Path.join(__dirname, flow)).toString()
 
     it(`'${name}' is a valid JSON`, function () {
       expect(() => JSON.parse(json)).to.not.throw()
     })
 
-
     it(`'${name}' has StartAt property`, function () {
       const workflow = JSON.parse(json)
       expect(workflow).to.have.ownProperty('StartAt')
+      expect(workflow.StartAt).is.a('string')
     })
-
-
-    it(`'${name}' has States property`, function () {
-      const workflow = JSON.parse(json)
-      expect(workflow).to.have.ownProperty('States')
-    })
-
 
     it(`'${name}' has Comment property`, function () {
       const workflow = JSON.parse(json)
       expect(workflow).to.have.ownProperty('Comment')
+      expect(workflow.Comment).is.a('string')
+    })
+
+    it(`'${name}' has States property`, function () {
+      const workflow = JSON.parse(json)
+      expect(workflow).to.have.ownProperty('States')
+      expect(workflow.States).is.a('object')
+    })
+
+    describe(`'${name}' State`, function () {
+      const states = JSON.parse(json).States
+
+      Object.keys(states).forEach((key) => {
+        it(`'${key}' has 'Type' property`, function () {
+          const state = states[key]
+          expect(state).to.have.ownProperty('Type')
+          expect(state.Type).is.a('string')
+        })
+
+        it(`'${key}' has 'Resource' property`, function () {
+          const state = states[key]
+          expect(state).to.have.ownProperty('Resource')
+          expect(state.Type).is.a('string')
+        })
+      })
     })
   })
 })

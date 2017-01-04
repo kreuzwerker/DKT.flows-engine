42a333e50f63d5ee53017e5a2c3f18151093970b
johdirr <johannesdirr@me.com>
2017-01-04 12:23:25 +0100

    enhance logger (verbosity)

--- cli/logger.js
+++ cli/logger.js
@@ -2,15 +2,19 @@ const colors = require('colors')
 
 
 function Logger(name, verbose = false) {
+  let _verbose = verbose
+
   return {
+    setVerbose: v => (_verbose = v),
+
     log: (...args) => {
-      if (verbose) {
+      if (_verbose) {
         console.log(`[${name}]`, ...args)
       }
     },
 
     success: (...args) => {
-      if (verbose) {
+      if (_verbose) {
         console.log(`[${name}]`, 'SUCCESS'.green, ...args)
       }
     },

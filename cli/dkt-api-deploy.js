8f946bcb455e704173fdf72e19a553a288dc2177
johdirr <johannesdirr@me.com>
2017-01-09 13:21:49 +0100

    enhance cli/dkt
    apiGateway

---
+++ cli/dkt-api-deploy.js
@@ -0,0 +1,28 @@
+const program = require('commander')
+const Logger = require('./logger')
+const ApiGateway = require('../lib/aws/apiGateway')
+
+
+/*
+ * ---- build description ------------------------------------------------------
+ */
+program._name = 'cli/dkt api deploy'
+program
+  .description('Update ApiGateway')
+  .option('-s, --stage <name>', 'stage to deploy')
+  .option('-v, --verbose', 'verbose output')
+  .parse(process.argv)
+
+
+const logger = Logger(program._name, program.verbose)
+const stage = program.stage
+
+if (!stage) {
+  return logger.error('Missing required stage parameter')
+}
+
+/*
+ * ---- build a single lambda function ---------------------------------------
+ */
+logger.log('START', 'Deploy ApiGateway')
+

18bc453c39d9a66eca08bb41dcc692877cf99c61
johdirr <johannesdirr@me.com>
2017-01-17 15:21:24 +0100

    trivial: add link to docs

--- src/services/fetchArticle/template.js
+++ src/services/fetchArticle/template.js
@@ -1,6 +1,10 @@
 const settings = require('../../../settings')
 
 
+/*
+ * AWS SAM Resource Template
+ * docs https://github.com/awslabs/serverless-application-model/blob/master/versions/2016-10-31.md#awsserverlessfunction
+ */
 module.exports = ({ key }) => ({
   FetchArticle: {
     Type: 'AWS::Serverless::Function',

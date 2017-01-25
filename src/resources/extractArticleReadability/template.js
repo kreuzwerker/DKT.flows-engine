b8d688623a4c7d790af97ed86f9a8a38c9cbe6a5
johipsum <johannesdirr@me.com>
2017-01-25 17:24:18 +0100

    don't use global runtime and timout settings for resources

--- src/resources/extractArticleReadability/template.js
+++ src/resources/extractArticleReadability/template.js
@@ -9,14 +9,14 @@ module.exports = ({ key }) => ({
   ExtractArticleReadability: {
     Type: 'AWS::Serverless::Function',
     Properties: {
-      Handler: settings.aws.lambda.handler,
-      Runtime: settings.aws.lambda.runtime,
+      Handler: 'index.handler',
+      Runtime: 'nodejs4.3',
       CodeUri: `s3://${settings.aws.s3.bucket}/${key}`,
       Policies: settings.aws.cloudFormation.policy,
-      Timeout: settings.aws.lambda.timeout,
+      Timeout: 20,
       Environment: {
         Variables: {
-          S3_BUCKET: 'dkt.flow-engine.test'
+          S3_BUCKET: settings.aws.s3.bucket
         }
       }
     }

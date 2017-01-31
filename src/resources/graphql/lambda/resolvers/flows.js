df191b0a4cc185b1e9610383657fb8aa081c99e0
johipsum <johannesdirr@me.com>
2017-01-31 17:51:16 +0100

    add create and update flow mutation resolvers

--- src/resources/graphql/lambda/resolvers/flows.js
+++ src/resources/graphql/lambda/resolvers/flows.js
@@ -1,5 +1,4 @@
-import { unmarshalItem } from 'dynamodb-marshaler'
-import dDB from '../../../../utils/dynamoDB'
+import { unmarshalItem, marshalItem } from 'dynamodb-marshaler'
 import dynDB from '../../../../utils/dynamoDB'
 
 
@@ -39,5 +38,62 @@ export function getFlowById(flowId) {
  * ---- Mutations --------------------------------------------------------------
  * -----------------------------------------------------------------------------
  */
+export async function createFlow(flow) {
+  const table = process.env.DYNAMO_FLOWS
+  const currentDate = new Date().toISOString()
+  const newFlow = Object.assign({}, flow, {
+    createdAt: currentDate,
+    updatedAt: currentDate
+  })
+  const params = {
+    Item: marshalItem(newFlow),
+    ReturnConsumedCapacity: 'TOTAL'
+  }
+
+  try {
+    const { ConsumedCapacity } = await dynDB.putItem(table, params)
+
+    if (ConsumedCapacity && ConsumedCapacity.CapacityUnits === 0) {
+      throw new Error('createFlow failed. No units consumed by the operation')
+    }
+    return newFlow
+  } catch (err) {
+    return Promise.reject(err)
+  }
+}
 
 // TODO
+export async function updateFlow(flow) {
+  const table = process.env.DYNAMO_FLOWS
+  const currentDate = new Date().toISOString()
+  const params = {
+    Key: marshalItem({ id: flow.id }),
+    ExpressionAttributeNames: {
+      '#uAt': 'updatedAt',
+      '#n': 'name',
+      '#d': 'description',
+      '#s': 'steps'
+    },
+    ExpressionAttributeValues: {
+      ':uAt': { S: currentDate },
+      ':n': { S: flow.name },
+      ':d': { S: flow.description },
+      ':s': { SS: flow.steps }
+    },
+    UpdateExpression: 'SET #uAt=:uAt, #n=:n, #d=:d, #s=:s',
+    ReturnConsumedCapacity: 'TOTAL'
+  }
+
+  try {
+    const res = await dynDB.updateItem(table, params)
+    return flow
+  } catch (err) {
+    return Promise.reject(err)
+  }
+}
+
+
+export function deleteFlow(flow) {
+  console.log(flow)
+  return Promise.resolve(flow)
+}

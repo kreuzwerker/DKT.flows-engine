b7268e59018611fa8668dc6df3921d61b48dfc40
johipsum <johannesdirr@me.com>
2017-02-09 17:00:44 +0100

    add graphql mutation test

--- src/resources/graphql/tests/test.js
+++ src/resources/graphql/tests/test.js
@@ -1,5 +1,6 @@
-// import { promisifyLambda } from '../../../../lib/promisifier'
-// import { handler } from '../lambda/index'
+import { astFromValue } from 'graphql'
+import { promisifyLambda } from '../../../../lib/promisifier'
+import { handler } from '../lambda/index'
 import seedTestdata from './seeder'
 // import event from './testEvents/event.json'
 import ResolversTests from './test.resolvers'
@@ -11,7 +12,7 @@ process.env.DYNAMO_SERVICES = 'DKT-flow-engine-Test-GraphQLDynamoServices-1P378K
 process.env.DYNAMO_STEPS = 'DKT-flow-engine-Test-GraphQLDynamoSteps-L5ZS4XOU9M6O'
 
 
-// const GraphQLLambda = promisifyLambda(handler)
+const GraphQLLambda = promisifyLambda(handler)
 
 
 describe('ƛ GraphQL', () => {
@@ -19,5 +20,46 @@ describe('ƛ GraphQL', () => {
     await seedTestdata()
   })
 
-  describe('Resolvers', ResolversTests)
+  // describe('Resolvers', ResolversTests)
+
+  describe('createStep', () => {
+    const createStepPayload = {
+      query: `
+        mutation createStep(
+          $pos: Int,
+          $desc: String,
+          $flow: ID,
+          $service: ID
+        ) {
+          createStep(
+            position: $pos,
+            description: $desc,
+            flow: $flow,
+            service: $service
+          ) {
+            id
+            position
+            description
+            flow {
+              id
+            }
+            service {
+              id
+            }
+          }
+        }
+      `,
+      operationName: 'createStep',
+      variables: {
+        pos: 1,
+        desc: 'Lorem Ipsum',
+        flow: 'dontDeleteMel0179imlh0a73',
+        service: 'dontDeleteMeh0133z9r38ljg'
+      }
+    }
+
+    it('works', async () => {
+      await GraphQLLambda({ body: createStepPayload, verbose: true })
+    })
+  })
 })

1151dc593ca981864773be6aa07da777807fed3f
johipsum <johannesdirr@me.com>
2017-02-16 11:19:09 +0100

    add response tests

--- src/resources/graphql/tests/test.js
+++ src/resources/graphql/tests/test.js
@@ -1,8 +1,6 @@
-import { astFromValue } from 'graphql'
 import { promisifyLambda } from '../../../../lib/promisifier'
 import { handler } from '../lambda/index'
 import seedTestdata from './seeder'
-// import event from './testEvents/event.json'
 import ResolversTests from './test.resolvers'
 
 
@@ -20,46 +18,81 @@ describe('ƛ GraphQL', () => {
     await seedTestdata()
   })
 
-  // describe('Resolvers', ResolversTests)
+  describe('Resolvers', ResolversTests)
 
-  describe('createStep', () => {
-    const createStepPayload = {
-      query: `
-        mutation createStep(
-          $pos: Int,
-          $desc: String,
-          $flow: ID,
-          $service: ID
-        ) {
-          createStep(
-            position: $pos,
-            description: $desc,
-            flow: $flow,
-            service: $service
-          ) {
-            id
-            position
-            description
-            flow {
-              id
-            }
-            service {
-              id
-            }
-          }
-        }
-      `,
-      operationName: 'createStep',
-      variables: {
-        pos: 1,
-        desc: 'Lorem Ipsum',
-        flow: 'dontDeleteMel0179imlh0a73',
-        service: 'dontDeleteMeh0133z9r38ljg'
+  describe('Responses', () => {
+    describe('when sending a valid payload', () => {
+      let response = {}
+      const validPayload = {
+        query: 'query FlowQuery($id: ID) { Flow(id: $id) { id name description } }',
+        operationName: 'FlowQuery',
+        variables: { id: 'dontDeleteMel0179imlh0a73' }
       }
-    }
 
-    it('works', async () => {
-      await GraphQLLambda({ body: createStepPayload, verbose: true })
+      before(async () => {
+        response = await GraphQLLambda({ body: validPayload, verbose: false })
+      })
+
+      it('with status code 200', () => {
+        expect(response).to.include.keys('statusCode')
+        expect(response.statusCode).to.equal(200)
+      })
+
+      it('with cors headers set', () => {
+        expect(response).to.include.keys('headers')
+        expect(response.headers).to.include.keys([
+          'Access-Control-Allow-Methods',
+          'Access-Control-Allow-Headers',
+          'Access-Control-Allow-Origin'
+        ])
+
+        expect(response.headers['Access-Control-Allow-Methods']).to.equal('DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT')
+        expect(response.headers['Access-Control-Allow-Headers']).to.equal('Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token')
+        expect(response.headers['Access-Control-Allow-Origin']).to.equal('*')
+      })
+
+      it('with a stringified json as response body', () => {
+        let json = {}
+        expect(() => (json = JSON.parse(response.body))).to.not.throw(Error)
+        expect(json).to.include.keys('data')
+      })
+    })
+
+    describe('when sending an invalid Payload', () => {
+      let response = {}
+      const invalidPayload = {
+        query: 'query FlowsQueries($id: ID) { Flows(id: $id) { id name description } }',
+        operationName: 'FlowsQueries',
+        variables: { id: 'dontDeleteMel0179imlh0a73' }
+      }
+
+      before(async () => {
+        response = await GraphQLLambda({ body: invalidPayload, verbose: false })
+      })
+
+      it('with status code 500', () => {
+        expect(response).to.include.keys('statusCode')
+        expect(response.statusCode).to.equal(500)
+      })
+
+      it('with cors headers set', () => {
+        expect(response).to.include.keys('headers')
+        expect(response.headers).to.include.keys([
+          'Access-Control-Allow-Methods',
+          'Access-Control-Allow-Headers',
+          'Access-Control-Allow-Origin'
+        ])
+
+        expect(response.headers['Access-Control-Allow-Methods']).to.equal('DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT')
+        expect(response.headers['Access-Control-Allow-Headers']).to.equal('Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token')
+        expect(response.headers['Access-Control-Allow-Origin']).to.equal('*')
+      })
+
+      it('with a stringified json as response body', () => {
+        let json = {}
+        expect(() => (json = JSON.parse(response.body))).to.not.throw(Error)
+        expect(json).to.include.keys('errors')
+      })
     })
   })
 })

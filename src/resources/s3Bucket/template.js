a1fb2bb10ee88d5a4d36017c22352c78a8090e42
johipsum <johannesdirr@me.com>
2017-02-21 14:50:02 +0100

    trivial: cleanup

--- src/resources/s3Bucket/template.js
+++ src/resources/s3Bucket/template.js
@@ -1,21 +1,11 @@
-const settings = require('../../../settings')
 const { S3_BUCKET } = require('../locicalResourceIds')
 
-/*
- * AWS SAM Resource Template
- * docs https://github.com/awslabs/serverless-application-model/blob/master/versions/2016-10-31.md#awsserverlessfunction
- */
-module.exports = ({ key, stage }) => ({
+
+module.exports = ({ stage }) => ({
   [S3_BUCKET]: {
     Type: 'AWS::S3::Bucket',
     Properties: {
       BucketName: `dkt.flow-engine.${stage.toLowerCase()}`
-      // CorsConfiguration: {
-      //   AllowedOrigins: ['*'],
-      //   AllowedMethods: ['GET'],
-      //   MaxAge: 3000,
-      //   AllowedHeaders: ['Authorization']
-      // }
     }
   }
 })

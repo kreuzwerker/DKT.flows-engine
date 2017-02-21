c9d85750e7100327433848d8a2d6a7ba980d53f3
johipsum <johannesdirr@me.com>
2017-02-21 15:59:12 +0100

    sort locical resources ids

--- src/resources/locicalResourceIds.js
+++ src/resources/locicalResourceIds.js
@@ -4,14 +4,8 @@
 
 module.exports = {
   S3_BUCKET: 's3Bucket',
-  EXTRACT_ARTICLE_DATE_FUNCTION: 'ExtractArticleDateFunction',
-  EXTRACT_ARTICLE_READABILITY_FUNCTION: 'ExtractArticleReadabilityFunction',
-  EXTRACT_ARTICLE_TEXT_FUNCTION: 'ExtractArticleTextFunction',
-  EXTRACT_ARTICLE_TITLE_FUNCTION: 'ExtractArticleTitleFunction',
 
   FETCH_ARTICLE_FUNCTION: 'FetchArticleFunction',
-
-  FLOW_RUNNER_FUNCTION: 'FlowRunnerFunction',
 
   GRAPHQL_FUNCTION: 'GraphQlFunction',
   GRAPHQL_API_GATEWAY: 'GraphQLApiGateway',
@@ -21,7 +15,16 @@ module.exports = {
   GRAPHQL_DB_SERVICES: 'GraphQLDynamoServices',
   GRAPHQL_DB_STEPS: 'GraphQLDynamoSteps',
 
-  MERGE_JSONS_FUNCTION: 'MergeJSONsFunction',
+  NLP_REQUEST_FUNCTION: 'NLPRequestFunction',
 
-  NLP_REQUEST_FUNCTION: 'NLPRequestFunction'
+
+  // ** legacy **
+  EXTRACT_ARTICLE_DATE_FUNCTION: 'ExtractArticleDateFunction',
+  EXTRACT_ARTICLE_READABILITY_FUNCTION: 'ExtractArticleReadabilityFunction',
+  EXTRACT_ARTICLE_TEXT_FUNCTION: 'ExtractArticleTextFunction',
+  EXTRACT_ARTICLE_TITLE_FUNCTION: 'ExtractArticleTitleFunction',
+
+  FLOW_RUNNER_FUNCTION: 'FlowRunnerFunction',
+
+  MERGE_JSONS_FUNCTION: 'MergeJSONsFunction'
 }

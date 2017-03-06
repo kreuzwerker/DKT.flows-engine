9a5d9389709db24b193af3fb7f85db58459b0ae7
johipsum <johannesdirr@me.com>
2017-03-06 15:18:08 +0100

    trigger function can now be part of a flowrun

--- src/resources/stateMachineTrigger/lambda/index.js
+++ src/resources/stateMachineTrigger/lambda/index.js
@@ -1,36 +1,18 @@
 import _isString from 'lodash/isString'
 import Logger from '../../../utils/logger'
-import S3 from '../../../utils/s3'
 import StepFunctions from '../../../utils/stepFunctions'
 
 
-/*
- * Trigger StateMachine
- *
- * This functions requires a json document on S3 with a stateMachone and a input key. e.g.
- * {
- *    stateMachine: "testMachine",
- *    input: {
- *      Key: "<bucket>/<key>"
- *    }
- * }
- */
 export async function handler(event, context, callback) {
-  const logger = Logger(event.verbose)
-  const s3 = S3(process.env.S3_BUCKET)
-
+  const logger = Logger()
+  const { stateMachine } = _isString(event) ? JSON.parse(event) : event
   try {
-    const { Key } = _isString(event) ? JSON.parse(event) : event
-    const { awsRequestId } = context
-
-    logger.log(`Get '${Key}' from '${s3.bucket}'`)
-    const data = await s3.getObject({ Key })
-
-    const { stateMachine, input } = JSON.parse(data.Body)
-    const response = await StepFunctions.startExecution(stateMachine, JSON.stringify(input))
-
+    logger.log(`Starting StateMachine: '${stateMachine}'`)
+    const response = await StepFunctions.startExecution(stateMachine, JSON.stringify(event))
+    logger.log(`Started StateMachine: '${stateMachine}'`, response)
     callback(null, JSON.stringify(response))
   } catch (err) {
+    logger.log(`Unable to start StateMachine: '${stateMachine}'`, err)
     callback(err)
   }
 }

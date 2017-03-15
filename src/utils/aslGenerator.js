ba41803ca1f6a244c0a649084826a984629d5564
johipsum <johannesdirr@me.com>
2017-03-15 14:27:57 +0100

    trivial: cleanup

--- src/utils/aslGenerator.js
+++ src/utils/aslGenerator.js
@@ -65,36 +65,3 @@ export default async function createASL(flowRun) {
     return Promise.reject(err)
   }
 }
-
-
-// const fakeFlowRun = {
-//   id: '7b32ec20-5d92-46f9-85c8-f6bb4efd292c',
-//   userId: '1',
-//   flow: {
-//     id: 'ciy0iehaj9wu20133m8gu0lju',
-//     name: 'SecondFlow',
-//     description: 'This, too, is a mocked flow object',
-//     steps: [
-//       {
-//         id: 'ciy0jgybhai7v013373puyuvy',
-//         position: 1,
-//         service: {
-//           id: 'ciy0jfkvuahth0133z9r38ljg',
-//           name: 'Fetch Article',
-//           arn: 'arn:aws:lambda:eu-west-1:855433257886:function:DKT-flow-engine-Test-FetchArticleFunction-16SWWTDVOY61Y',
-//           type: 'ACTION'
-//         }
-//       },
-//       {
-//         id: 'ciy0jidbmaivt0133sqdy35e6',
-//         position: 0,
-//         service: {
-//           id: 'ciy0jeruzbb5m01790ymkz7xl',
-//           name: 'URL_INPUT',
-//           arn: null,
-//           type: 'TRIGGER'
-//         }
-//       }
-//     ]
-//   }
-// }

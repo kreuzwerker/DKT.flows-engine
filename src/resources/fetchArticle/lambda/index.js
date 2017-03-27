3ad67f5e00c68f6393f882d21ec8ed5027e2f244
johipsum <johannesdirr@me.com>
2017-03-27 11:30:01 +0200

    use step test success handler on testStep event

--- src/resources/fetchArticle/lambda/index.js
+++ src/resources/fetchArticle/lambda/index.js
@@ -1,22 +1,22 @@
 import fetch from 'node-fetch'
 import _isString from 'lodash/isString'
 import Logger from '../../../utils/logger'
-import { getFlowRunData, flowRunStepSuccessHandler } from '../../../utils/helpers/flowRunHelpers'
-
+import { flowRunStepSuccessHandler } from '../../../utils/helpers/flowRunHelpers'
+import { getStepData, testStepSuccessHandler } from '../../../utils/helpers/stepHelpers'
 
 /*
  * Fetch an Article from given URL
  */
 export async function handler(event, context, callback) {
-  console.log('** CLIENT CONTEXT **', JSON.stringify(event, null, 2))
-
   const logger = Logger(event.verbose)
   const input = _isString(event) ? JSON.parse(event) : event
+  let output = {}
+
   input.currentStep += 1
 
   try {
-    const flowRunData = await getFlowRunData(input)
-    const inputData = flowRunData[input.contentKey]
+    const stepData = await getStepData(input)
+    const inputData = stepData[input.contentKey]
 
     logger.log(`Try to fetch data from ${inputData}`)
     const result = await fetch(inputData)
@@ -29,10 +29,16 @@ export async function handler(event, context, callback) {
     let articleHTML = await result.text()
     articleHTML = articleHTML.replace(/(\r\n|\n|\r|\t)/gm, '')
 
-    const output = await flowRunStepSuccessHandler(input, flowRunData, articleHTML)
+    if (input.testStep) {
+      output = await testStepSuccessHandler(input, stepData, articleHTML)
+    } else {
+      output = await flowRunStepSuccessHandler(input, stepData, articleHTML)
+    }
+
+    console.log('STRINGIFIED OUTPUT', output)
     callback(null, output)
   } catch (err) {
-    const output = Object.assign({}, input, { error: err })
+    output = Object.assign({}, input, { error: err })
     callback(null, output)
   }
 }

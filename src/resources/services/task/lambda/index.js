91a012dc7927c1202e6d3047b2f4ffa84f8257bf
johipsum <johannesdirr@me.com>
2017-09-01 10:30:11 +0200

    add task lambda

--- src/resources/services/task/lambda/index.js
+++ src/resources/services/task/lambda/index.js
@@ -1,6 +1,21 @@
-// THIS IS A TAKS WORKER. NOT A REGULAR SERVICE LAMBDA.
-// A TAKS WORKER CANNOT BE PART OF A STATEMACHINE
-export function handler(inputData, logger) {
-  logger.log(inputData)
-  return 'foobar'
+import _isString from 'lodash/isString'
+import StepFunctions from '../../../../utils/stepFunctions'
+
+// THIS IS A TASK WORKER. NOT A REGULAR SERVICE LAMBDA.
+// A TASK WORKER CANNOT BE PART OF A STATEMACHINE
+
+export async function handler(event, context, callback) {
+  const inputData = _isString(event) ? JSON.parse(event) : event
+  const { activityArn } = inputData
+
+  try {
+    const { taskToken, input } = await StepFunctions.getActivityTask({ activityArn })
+    await StepFunctions.sendTaskSuccess({
+      taskToken,
+      output: input
+    })
+    callback(null, input)
+  } catch (err) {
+    callback(err)
+  }
 }

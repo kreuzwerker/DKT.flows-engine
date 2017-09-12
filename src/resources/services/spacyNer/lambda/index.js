63990a3775a8868f7597cb1205d1c87f5660652c
Markus Herzog <markus@markusherzog.com>
2017-09-12 11:22:07 +0200

    Add a service for entity recognition with spacy

---
+++ src/resources/services/spacyNer/lambda/index.js
@@ -0,0 +1,40 @@
+import fetch from 'node-fetch'
+import service from '../../../../utils/service'
+
+/*
+ * Named Entity Recognition with spacy
+ */
+async function spacyNer(inputData, logger) {
+  const { SPACY_SERVICE_URL } = process.env
+
+  let nerResult = {},
+      nerJSON = {}
+
+  try {
+    nerResult = await fetch(SPACY_SERVICE_URL, {
+      method: 'POST',
+      body: JSON.stringify({
+        text: inputData,
+        model: 'en_core_web_sm',
+        o: 'json'
+      })
+    })
+    if (!nerResult.ok) {
+      return Promise.reject(`Failed fetching ${SPACY_SERVICE_URL} - ${nerResult.status} ${nerResult.statusText}`)
+    }
+  } catch (error) {
+    logger.log('Error while requesting result from spacy-ner', error)
+    return Promise.reject(error)
+  }
+
+  try {
+    nerJSON = await nerResult.json()
+  } catch (error) {
+    return Promise.reject(error)
+  }
+  return nerJSON
+}
+
+
+export const handler = service(spacyNer)
+//export const handler = spacyNer // TODO: service() doesn't work well with the tests
---
+++ src/resources/services/spacyNer/service.js
@@ -0,0 +1,14 @@
+const { SPACY_NER_FUNCTION } = require('../../locicalResourceIds')
+
+module.exports = arn => ({
+  description: 'Named Entity Recognition with spacy',
+  icon: 'rss_feed',
+  id: 'DTFARUqXHqbLasX3qnee',
+  locicalResourceId: SPACY_NER_FUNCTION,
+  name: 'Spacy NER Service',
+  provider: 'ciy0ivuazabar0133xu1vlvyj',
+  type: 'ACTION',
+  configSchema: null,
+  samplePayload: 'Apple is looking at buying U.K. startup for $1 billion',
+  arn: arn
+})

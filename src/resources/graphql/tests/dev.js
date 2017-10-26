0306047702b433635fd86d1212de452acc810bbe
Alex Wilde <alexosoft@gmail.com>
2017-10-26 10:22:34 +0700

    Added helpers for local testing.

---
+++ src/resources/graphql/tests/dev.js
@@ -0,0 +1,15 @@
+/*
+  For use in local development environment
+ */
+
+process.env.DYNAMO_FLOW_RUNS = 'DKT-flow-engine-Dev-DynamoDBFlowRuns-1K71Q4RBGISAT'
+process.env.DYNAMO_FLOWS = 'DKT-flow-engine-Dev-DynamoDBFlows-SBA5FMVQ4TWQ'
+process.env.DYNAMO_PROVIDERS = 'DKT-flow-engine-Dev-DynamoDBProviders-J2CXKJK3TOIW'
+process.env.DYNAMO_SERVICES = 'DKT-flow-engine-Dev-DynamoDBServices-XZN4XY9Z25VH'
+process.env.DYNAMO_STEPS = 'DKT-flow-engine-Dev-DynamoDBSteps-P4IK5Y04R5A0'
+process.env.DYNAMO_SERVICE_FEEDS = 'DKT-flow-engine-Dev-DynamoDBFlowRuns-1K71Q4RBGISAT'
+process.env.DYNAMO_TASKS = 'DKT-flow-engine-Dev-DynamoDBTasks-1BDS5ZFY34H6T'
+
+process.env.S3_BUCKET = 'dkt.flow-engine.dev'
+process.env.STATE_MACHINE_TRIGGER_FUNCTION =
+  'DKT-flow-engine-Dev-StateMachineTriggerFunction-1N5K2FVI8NY5D'

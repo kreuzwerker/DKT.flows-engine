ca65059c82e59f85503af54591ed8f6c8276c7e6
johipsum <johannesdirr@me.com>
2017-11-10 17:41:27 +0100

    add inputType and outputType to graphql serviceType

--- src/resources/graphql/lambda/types/service.js
+++ src/resources/graphql/lambda/types/service.js
@@ -39,6 +39,8 @@ export const ServiceType = new GraphQLObjectType({
     name: { type: GraphQLString },
     description: { type: GraphQLString },
     type: { type: GraphQLString },
+    inputType: { type: GraphQLString },
+    outputType: { type: GraphQLString },
     scheduled: { type: GraphQLBoolean },
     task: { type: GraphQLBoolean },
     taskType: { type: GraphQLString },

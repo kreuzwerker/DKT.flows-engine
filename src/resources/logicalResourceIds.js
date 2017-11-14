7f2d600a17d32f6ffc391ca34be3b3142e7f2da9
johipsum <johannesdirr@me.com>
2017-11-14 16:32:42 +0100

    add s3Output service definition and template

--- src/resources/logicalResourceIds.js
+++ src/resources/logicalResourceIds.js
@@ -29,6 +29,8 @@ module.exports = {
 
   CAPITALIZE_SERVICE_FUNCTION: 'CapitalizeServiceFunction',
 
+  S3_OUTPUT_SERVICE_FUNCTION: 'S3OutputServiceFunction'
+
   APPROVE_TASK_FUNCTION: 'ApproveTaskFunction',
   REVIEW_TASK_FUNCTION: 'ReviewTaskFunction',
   MODIFY_TASK_FUNCTION: 'ModifyTaskFunction',
---
+++ src/resources/services/s3Output/lambda/index.js
@@ -0,0 +1,8 @@
+import service from '../../../../utils/service'
+
+function s3Output(inputData, logger) {
+  // TODO
+  return 'done'
+}
+
+export const handler = service(s3Output)

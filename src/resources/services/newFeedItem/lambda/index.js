fbb8c4ce9776959869c9e039d94d2a7e516d2c99
johipsum <johannesdirr@me.com>
2017-11-14 12:14:30 +0100

    fix lambda handler
    and make it shorter and faster

--- src/resources/services/newFeedItem/lambda/index.js
+++ src/resources/services/newFeedItem/lambda/index.js
@@ -42,80 +42,52 @@ function getNewFeedItems(params) {
   })
 }
 
-export async function handler(event, context, callback) {
-  console.log('newItemInFeed', event)
+export function handler(event, context, callback) {
   event.verbose = true
   const logger = Logger(event.verbose)
   const input = _isString(event) ? JSON.parse(event) : event
+  const url = input.configParams.find(param => param.fieldId === 'url').value
 
-  console.log(JSON.stringify(input, null, 2))
+  Promise.all([
+    dbFlowRuns.getFlowRunById(input.flowRun.id),
+    getNewFeedItems({ flowRunId: input.flowRun.id, url: url })
+  ])
+    .then(([flowRun, items]) => {
+      if (items.length === 0) {
+        return dbFlowRuns.updateFlowRun({
+          id: input.flowRun.id,
+          status: 'success',
+          message: 'No new feed items to process.'
+        })
+      }
 
-  const url = input.configParams.reduce((a, param) => {
-    return param.fieldId === 'url' ? param.value : a
-  }, '')
-
-  function err(error) {
-    const result = Object.assign({}, input, { error })
-    logger.log('Error:', error)
-    callback(null, result)
-  }
-
-  let items = []
-  try {
-    items = await getNewFeedItems({ flowRunId: input.flowRun.id, url: url }, logger)
-  } catch (error) {
-    err(error)
-    return
-  }
-
-  logger.log('DEBUG feed items', items)
-  if (!items.length) {
-    const msg = 'No new feed items to process.'
-    logger.log(msg)
-
-    try {
-      const result = await dbFlowRuns.updateFlowRun({
-        id: input.flowRun.id,
-        status: 'success',
-        message: msg
-      })
-
-      logger.log(`Trigger FlowRun '${input.flowRun.id}'`)
+      return Promise.all(items.map((item) => {
+        logger.log(`Trigger FlowRun with URL ${item.link}`)
+        return triggerFlowRun(flowRun, item.link)
+      }))
+    })
+    .then((res) => {
+      callback(null, res)
+    })
+    .catch((error) => {
+      const result = { ...input, error }
+      logger.log('Error:', error)
       callback(null, result)
-      return
-    } catch (error) {
-      err(error)
-      return
-    }
-  }
-
-  logger.log(`Trigger FlowRun '${input.flowRun.id}'`)
-  try {
-    if (!items[0].url) {
-      err('Feed item has no URL property.')
-      return
-    }
-
-    const result = await triggerFlowRun(input.flowRun, items[0].url)
-    logger.log(`Triggered FlowRun with URL ${items[0].url}`, result)
-    callback(null, result)
-  } catch (error) {
-    err(error)
-  }
+    })
 }
 
 // Test function locally
-process.env.DYNAMO_SERVICE_FEEDS = 'DKT-flow-engine-Dev-DynamoDBServiceFeeds-1FLY1RDH4BGRD'
-getNewFeedItems(
-  {
-    flowId: '1',
-    url: 'https://news.ycombinator.com/rss'
-  },
-  console
-)
-  .then((res) => {
-    // console.log(res)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+// process.env.DYNAMO_SERVICE_FEEDS = 'DKT-flow-engine-Dev-DynamoDBServiceFeeds-1FLY1RDH4BGRD'
+// getNewFeedItems(
+//   {
+//     flowId: '1',
+//     url: 'https://news.ycombinator.com/rss'
+//   },
+//   console
+// )
+//   .then((res) => {
+//     // console.log(res)
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })

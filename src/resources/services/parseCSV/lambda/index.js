91f511f7182e6581e3aa1cd997321ee88f8b7eee
johipsum <johannesdirr@me.com>
2017-11-15 18:23:17 +0100

    introduce first version of the csvParseServive

--- src/resources/services/parseCSV/lambda/index.js
+++ src/resources/services/parseCSV/lambda/index.js
@@ -1,15 +1,27 @@
 import csvParse from 'csv-parse'
 import service from '../../../../utils/service'
 
-  // TODO
+function parse(csv, opts) {
+  return new Promise((resolve, reject) => {
+    csvParse(csv, opts, (err, res) => {
+      if (err) {
+        return reject(err)
+      }
+      return resolve(res)
+    })
+  })
+}
+
 function parseCSV(inputData, { configParams }, logger) {
-  const separator = configParams.get('separator')
-  const includeHeader = configParams.get('header')
+  const delimiter = configParams.get('separator')
+  const columns = !!configParams.get('header')
 
-  logger.log('separator:', separator)
-  logger.log('includeHeader:', includeHeader)
+  logger.log('Separator:', delimiter)
+  logger.log('Iclude Header:', columns)
 
-  return Promise.resolve(inputData)
+  // TODO clarify what "include header?" means for the result
+
+  return parse(inputData, { delimiter, columns })
 }
 
 export const handler = service(parseCSV)

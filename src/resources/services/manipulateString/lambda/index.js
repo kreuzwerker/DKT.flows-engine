44fdd0d772ceac2939c3c49e0e7ce0a48bb01467
johipsum <johannesdirr@me.com>
2017-11-17 16:31:05 +0100

    add manipulate service definition

---
+++ src/resources/services/manipulateString/lambda/index.js
@@ -0,0 +1,8 @@
+import service from '../../../../utils/service'
+
+function manipulateSring(inputData, { configParams }) {
+  // TODO
+  return inputData
+}
+
+export const handler = service(manipulateSring)
---
+++ src/resources/services/manipulateString/service.js
@@ -0,0 +1,18 @@
+const { MANIPULATE_STRING_FUNCTION } = require('../../logicalResourceIds')
+
+module.exports = arn => ({
+  description: 'Manipulate String',
+  icon: 'insert_drive_file',
+  id: 'QvTv9lsZcWpo425eQfiHYnPN',
+  logicalResourceId: MANIPULATE_STRING_FUNCTION,
+  name: 'Manipulate String',
+  provider: 'xOMFBmrwQmul4MB8FnEH3Z1b',
+  type: 'ACTION',
+  inputType: 'string',
+  outputType: 'string',
+  scheduled: null,
+  configSchema: [],
+  samplePayload:
+    'Lorem ipsum dolor sit amet consectetur adipiscing elit tollitur beneficium tollitur gratia quae sunt vincla concordiae quod quidem iam fit etiam in academia cur tantas.',
+  arn: arn
+})

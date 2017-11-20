5844f5dc6536ec75dd12ee857964779c4b01d3f7
johipsum <johannesdirr@me.com>
2017-11-20 18:08:19 +0100

    manipulate string with lodash functions

--- src/resources/services/manipulateString/lambda/index.js
+++ src/resources/services/manipulateString/lambda/index.js
@@ -1,8 +1,29 @@
+import _str from 'lodash/string'
 import service from '../../../../utils/service'
 
+function parseBoolean(val) {
+  if (typeof val === 'boolean') return val
+  return val === 'true'
+}
+
 function manipulateSring(inputData, { configParams }) {
-  // TODO
-  return inputData
+  let str = inputData
+
+  configParams.forEach((param) => {
+    if (parseBoolean(configParams.get(param.fieldId)) === true) {
+      str = _str[param.fieldId](str)
+    }
+  })
+
+  return Promise.resolve(str) // for now
 }
 
 export const handler = service(manipulateSring)
+
+// const configParams = [
+//   { fieldId: 'camelCase', value: 'true' },
+//   { fieldId: 'capitalize', value: 'true' }
+// ]
+// configParams.get = fieldId => configParams.find(t => t.fieldId === fieldId).value
+//
+// manipulateSring('foobar', { configParams })

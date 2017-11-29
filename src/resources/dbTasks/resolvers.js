e0c3385d9327e97e844302a204cf7884a09724b8
johipsum <johannesdirr@me.com>
2017-11-29 11:21:01 +0100

    fix allTasks query [#113]

--- src/resources/dbTasks/resolvers.js
+++ src/resources/dbTasks/resolvers.js
@@ -1,5 +1,6 @@
 import { DynamoDB } from '../../utils/aws'
 
+// TODO we should paginate this!
 export function allTasks(userId) {
   // Retrieves all unfinished tasks
   const table = process.env.DYNAMO_TASKS
@@ -17,7 +18,21 @@ export function allTasks(userId) {
     }
   }
 
-  return DynamoDB.scan(table, params).then(r => r.Items)
+  const tasks = []
+
+  function scan(args) {
+    return DynamoDB.scan(table, args).then(({ Items, LastEvaluatedKey }) => {
+      Items.forEach(item => tasks.push(item))
+      if (LastEvaluatedKey) {
+        // scan the next set
+        return scan({ ...params, ExclusiveStartKey: LastEvaluatedKey })
+      }
+
+      return Promise.resolve()
+    })
+  }
+
+  return scan(params).then(() => tasks)
 }
 
 export function getTaskById(taskId, userId) {

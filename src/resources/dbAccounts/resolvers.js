a84cfc6ae51e8687d1bf086f03d98d874403653b
johipsum <johannesdirr@me.com>
2017-11-30 13:54:58 +0100

    implement get resolver

---
+++ src/resources/dbAccounts/resolvers.js
@@ -0,0 +1,31 @@
+import { DynamoDB } from '../../utils/aws'
+
+export function allAccounts(userId) {
+  const table = process.env.DYN_DB_ACCOUNTS
+  const params = {
+    FilterExpression: '#userId = :userId',
+    ExpressionAttributeNames: {
+      '#userId': 'userId'
+    },
+    ExpressionAttributeValues: {
+      ':userId': userId
+    }
+  }
+
+  return DynamoDB.scan(table, params).then(r => r.Items)
+}
+
+export function getAccountById(id, userId) {
+  const table = process.env.DYN_DB_ACCOUNTS
+  const query = {
+    Key: { id }
+  }
+
+  return DynamoDB.getItem(table, query).then((r) => {
+    const item = r.Item || {}
+    return typeof userId === 'undefined' || item.userId === userId || item.userId === null
+      ? item
+      : null
+  })
+}
+

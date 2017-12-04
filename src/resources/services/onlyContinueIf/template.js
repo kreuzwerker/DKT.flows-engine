7e599a8b30f0df006c373d82378ef85a169d731d
johipsum <johannesdirr@me.com>
2017-12-04 13:59:10 +0100

    fix typo

--- src/resources/services/onlyContinueIf/template.js
+++ src/resources/services/onlyContinueIf/template.js
@@ -3,7 +3,7 @@ const { ONLY_CONTINUE_IF_FUNCTION, S3_BUCKET } = require('../../logicalResourceI
 
 module.exports = ({ key }) => ({
   [ONLY_CONTINUE_IF_FUNCTION]: {
-    Type: 'AWS::Serverless:Function',
+    Type: 'AWS::Serverless::Function',
     Properties: {
       Handler: 'index.handler',
       Runtime: 'nodejs6.10',
